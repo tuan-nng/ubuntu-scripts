9e8bbf151cf6a38eb0d9b3dfeadcc3dc67b67d6b
novpla <tuannn.bk@gmail.com>
2019-03-31 22:18:45 +0200

    add install zsh script

---
+++ install-zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install zsh -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
